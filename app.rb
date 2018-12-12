08cb49a16228d7915c9d754715f0248e4078da32
vagrant <vagrant@kleer-csd-box>
2018-12-12 16:53:07 -0300

    hola

--- app.rb
+++ app.rb
@@ -1,5 +1,34 @@
+
+
 require 'sinatra'
+
 
 get '/' do
     
+end
+
+enable :sessions
+get '/juego' do
+	#@palabra = params[:palabra]
+	@palabra = "basura"
+	if @palabra.length == 1
+
+		erb :juego
+	else
+		session[:palabra] = @palabra.chars
+		session[:longitud] = @palabra.length
+		session[:lista] = []
+		session[:palabra].each do |elemento|
+			session[:lista].push("_")
+		end
+		erb :juego
+	end 
+end
+
+
+
+#prueba para comprobar la session
+enable :sessions
+get '/vaina' do
+	session[:palabra]
 end
